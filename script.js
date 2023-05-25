9b7f34efdae9df6c3d88ab09fdbdf67fb6f278d9
Ruan789 <129328251+Ruan789@users.noreply.github.com>
2023-05-25 11:46:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function calcMedia(){
     let nomeAluno = window.prompt('Digite o seu nome')
-    let nota1 = Number(window.prompt('Insira a nota 1:'))
-    let nota2 = Number(window.prompt('Insira a nota 2:'))
-    let nota3 = Number(window.prompt('Insira a nota 3:'))
-    let media = (nota1 + nota2 + nota3)/3
-    document.getElementById('showMedia').innerHTML = media
-    if (media < 7)
+    let nota1Window = Number(window.prompt('Insira a nota 1:'))
+    let nota2Window = Number(window.prompt('Insira a nota 2:'))
+    let nota3Window = Number(window.prompt('Insira a nota 3:'))
+    let mediaWindow = (nota1Window + nota2Window + nota3Window)/3
+    document.getElementById('showMedia').innerHTML = mediaWindow
+    if (mediaWindow < 6)
         document.getElementById('showAprov').innerHTML = `O aluno ${nomeAluno} foi reprovado`
     else
         document.getElementById('showAprov').innerHTML = `O aluno ${nomeAluno} foi aprovado`
+}
+function calcInputMedia(){
+    let nomeAluno = document.getElementById('nomeInput').value
+    let nota1Input = Number(document.getElementById("valor1").value)
+    let nota2Input = Number(document.getElementById('valor2').value)
+    let nota3Input = Number(document.getElementById('valor3').value)
+    if (nota1Input > 10)
+        nota1Input = 10
+    else{}
+    if (nota2Input > 10)
+        nota2Input = 10
+    else{}
+    if (nota3Input > 10)
+        nota3Input = 10
+    else{}
+    let mediaInput = (nota1Input + nota2Input + nota3Input)/3
+    if (mediaInput < 6)
+        document.getElementById('showAprov').innerHTML = `O aluno ${nomeAluno} foi reprovado, média = ${mediaInput}`
+    else
+        document.getElementById('showAprov').innerHTML = `O aluno ${nomeAluno} foi aprovado, média = ${mediaInput}`
 }
